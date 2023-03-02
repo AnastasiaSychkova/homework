676d62aefb3f0d80d9002df0dd7c639dd213bc3d
Anastasia <sychkova.nastyusha@mail.ru>
2023-03-02 20:57:47 +0400

    дз исправлено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        finish("oooooooooooo", "jfjkjkgR55", "jfjkjkgR");
+        finish("oooooooooooo", "bggbg655", "bggbg655");
     }
+
+    public static boolean finish(String login, String password, String repeatPassword) {
+        try {
+            checkLogin(login);
+            checkPassword(password, repeatPassword);
+            return true;
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    private static void checkLogin(String login) {
+        if (isValidLength(login) && hasOnlyValidSymbols(login)) {
+            System.out.println("Верный логин");
+        } else {
+            throw new WrongLoginException("Неправильный логин");
+        }
+    }
+
+    private static void checkPassword(String password, String repeatPassword) {
+        if (hasOnlyValidSymbols(password) && equalsPassword(password, repeatPassword) && isValidLength(password)) {
+            System.out.println("Верный пароль");
+        } else {
+            throw new WrongPasswordException("Неправильный пароль");
+        }
+    }
+
+    private static boolean equalsPassword(String pass1, String pass2) {
+        return pass1.equals(pass2);
+    }
+
+    private static boolean hasOnlyValidSymbols(String s) {
+        final String alphabet = "abcdefghigklmnopqrstuvwxyz0123456789_";
+        for (int i = 0; i < s.length(); i++) {
+            if (!alphabet.contains(String.valueOf(s.charAt(i)).toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValidLength(String s) {
+        return s.length() <= 20;
+    }
+
 }
